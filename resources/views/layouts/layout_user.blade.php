4bf4ddf33b2e0e3980b9916cebe12d7cc8cf886b
alfajri23 <123190088@student.upnyk.ac.id>
2022-08-19 11:53:22 +0700

    sw

--- resources/views/layouts/layout_user.blade.php
+++ resources/views/layouts/layout_user.blade.php
@@ -48,7 +48,7 @@
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
-    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
 
   </head>
 
@@ -104,10 +104,10 @@
         //End number
     </script>
 
-    <script src="{{ asset('/sw.js') }}"></script>
+    <script src="{{ asset('sw.js') }}"></script>
     <script>
         if (!navigator.serviceWorker.controller) {
-            navigator.serviceWorker.register("/sw.js").then(function (reg) {
+            navigator.serviceWorker.register("{{ asset('sw.js') }}").then(function (reg) {
                 console.log("Service worker has been registered for scope: " + reg.scope);
             });
         }
