fd3cf8947af7f3cf3c396d2ec6dae131eb7255cf
alfajri23 <123190088@student.upnyk.ac.id>
2023-07-15 10:38:16 +0700

    galeri fixing

--- resources/views/pages/home/user/home_travel.blade.php
+++ resources/views/pages/home/user/home_travel.blade.php
@@ -401,6 +401,7 @@
 @include('components.galeri.galeri_2')
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fslightbox/3.3.3/index.min.js" integrity="sha512-qPiIthBLNh6rmNHxCsoGQcTwjqUUjPfoBi/PaSz/wraBWJeSTG8bBb0PkVx2q7OpxoB2L7EhCXQITLswZPy1BQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     const observer = lozad(); // lazy loads elements with default selector as '.lozad'
     observer.observe();
